f828b8ad7accd6a7cd1b7423ab8dc35f3c39912b
Stef Arntz <502348@vistacollege.nl>
2023-12-07 15:14:26 +0200

    changes

--- src/app/components/itemcard.tsx
+++ src/app/components/itemcard.tsx
@@ -3,15 +3,22 @@ import React from "react";
 
 interface ItemCardProps {
   imageSrc: string | StaticImageData;
+  imageScrCamo: string | StaticImageData;
   title: string;
   description: string;
 }
 
-const ItemCard: React.FC<ItemCardProps> = ({ imageSrc, title }) => {
+const ItemCard: React.FC<ItemCardProps> = ({
+  imageSrc,
+  imageScrCamo,
+  title,
+  description,
+}) => {
   return (
     <div className="max-w-md w-full rounded-2xl overflow-hidden drop-shadow-lg bg-neutral-700">
       <div className="px-3 py-2">
-        <div className="font-bold text-l mb-2">{title}</div>
+        <div className="font-bold text-l">{title}</div>
+        <div className="text-xs">{description}</div>
       </div>
 
       <div className="relative w-full aspect-[10/3]">
@@ -19,10 +26,19 @@ const ItemCard: React.FC<ItemCardProps> = ({ imageSrc, title }) => {
           src={imageSrc}
           alt={title}
           fill
-          className="object-center object-contain p-5 rounded-t"
+          className="object-center object-contain p-2"
         />
       </div>
-      <div></div>
+
+      <div className="px-3 py-3 w-full flex justify-center flex-wrap gap-3">
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+        <Image src={imageScrCamo} alt={title} width={40} height={40} />
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import ItemCard from "./components/itemcard";
-import gun1 from "../assets/dg-58-call-of-duty-mw3.png";
-import gun2 from "../assets/fr_556-call-of-duty-mw3.png";
-import gun3 from "../assets/holger_556-call-of-duty-mw3.png";
-import gun4 from "../assets/mcw-call-of-duty-mw3.png";
-import gun5 from "../assets/mtz-556-call-of-duty-mw3.png";
-import gun6 from "../assets/sva-45-call-of-duty-mw3.png";
-import gun7 from "../assets/amr9-call-of-duty-mw3.png";
-import gun8 from "../assets/rival-9-call-of-duty-mw3.png";
-import gun9 from "../assets/striker-call-of-duty-mw3.png";
-import gun10 from "../assets/striker-9-call-of-duty-mw3.png";
-import gun11 from "../assets/wsp_swarm-call-of-duty-mw3.png";
-import gun12 from "../assets/wsp-9-call-of-duty-mw3.png";
+import gun1 from "../assets/guns/dg-58-call-of-duty-mw3.png";
+import gun2 from "../assets/guns/fr_556-call-of-duty-mw3.png";
+import gun3 from "../assets/guns/holger_556-call-of-duty-mw3.png";
+import gun4 from "../assets/guns/mcw-call-of-duty-mw3.png";
+import gun5 from "../assets/guns/mtz-556-call-of-duty-mw3.png";
+import gun6 from "../assets/guns/sva-45-call-of-duty-mw3.png";
+import gun7 from "../assets/guns/amr9-call-of-duty-mw3.png";
+import gun8 from "../assets/guns/rival-9-call-of-duty-mw3.png";
+import gun9 from "../assets/guns/striker-call-of-duty-mw3.png";
+import gun10 from "../assets/guns/striker-9-call-of-duty-mw3.png";
+import gun11 from "../assets/guns/wsp_swarm-call-of-duty-mw3.png";
+import gun12 from "../assets/guns/wsp-9-call-of-duty-mw3.png";
+import camo1 from "../assets/camo/fractal-dirt.png";
 
 export default function Home() {
   return (
@@ -18,34 +19,94 @@ export default function Home() {
       <div className="mt-20">
         {/* itemcards ar */}
         <div className="p-4 text-l font-bold ">Assault rifles</div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 w-full justify-center">
-          <ItemCard imageSrc={gun5} title="Mtz-556" description="ar" />
+        <div className="grid grid-cols-3 gap-5 w-full justify-center">
+          <ItemCard
+            imageSrc={gun5}
+            title="mtz-556"
+            description="ar"
+            imageScrCamo={camo1}
+          />
 
-          <ItemCard imageSrc={gun4} title="Mcw" description="ar" />
+          <ItemCard
+            imageSrc={gun4}
+            title="mcw"
+            description="ar"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun6} title="Sva-45" description="ar" />
+          <ItemCard
+            imageSrc={gun6}
+            title="sva-45"
+            description="ar"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun1} title="Dg-58" description="ar" />
+          <ItemCard
+            imageSrc={gun1}
+            title="dg-58"
+            description="ar"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun2} title="Fr-556" description="ar" />
+          <ItemCard
+            imageSrc={gun2}
+            title="fr-556"
+            description="ar"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun3} title="Holger-556" description="ar" />
+          <ItemCard
+            imageSrc={gun3}
+            title="holger-556"
+            description="ar"
+            imageScrCamo={""}
+          />
         </div>
 
         {/* itemcards smg */}
         <div className="p-4 text-l font-bold ">Submachine guns</div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 w-full justify-center">
-          <ItemCard imageSrc={gun7} title="Amr9" description="smg" />
+        <div className="grid grid-cols-3 gap-5 w-full justify-center">
+          <ItemCard
+            imageSrc={gun7}
+            title="amr9"
+            description="smg"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun8} title="Rival-9" description="smg" />
+          <ItemCard
+            imageSrc={gun8}
+            title="rival-9"
+            description="smg"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun9} title="Striker" description="smg" />
+          <ItemCard
+            imageSrc={gun9}
+            title="striker"
+            description="smg"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun10} title="Striker-9" description="smg" />
+          <ItemCard
+            imageSrc={gun10}
+            title="striker-9"
+            description="smg"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun11} title="Wsp-swarm" description="smg" />
+          <ItemCard
+            imageSrc={gun11}
+            title="wsp-swarm"
+            description="smg"
+            imageScrCamo={""}
+          />
 
-          <ItemCard imageSrc={gun12} title="Wsp-9" description="smg" />
+          <ItemCard
+            imageSrc={gun12}
+            title="wsp-9"
+            description="smg"
+            imageScrCamo={""}
+          />
         </div>
       </div>
     </main>
