0f1a323173c56e9fcf8dfa78554d8d2c996bae38
Stef Arntz <502348@vistacollege.nl>
2023-12-08 15:21:24 +0200

    comments

--- src/app/images.tsx
+++ src/app/images.tsx
@@ -1,26 +1,32 @@
+// sva-45
 import sva45 from "../assets/guns/ar/sva-45/sva-45-call-of-duty-mw3.png";
 import bluetones from "../assets/guns/ar/sva-45/blue-tones.png";
 import topomuddy from "../assets/guns/ar/sva-45/topo-muddy.png";
 import cinders from "../assets/guns/ar/sva-45/cinders.png";
 import monarch from "../assets/guns/ar/sva-45/monarch.png";
-
+// dg-58
 import dg58 from "../assets/guns/ar/dg-58/dg-58-call-of-duty-mw3.png";
+// fr-556
 import fr556 from "../assets/guns/ar/fr-556/fr_556-call-of-duty-mw3.png";
+// holger-556
 import holger556 from "../assets/guns/ar/holger-556/holger_556-call-of-duty-mw3.png";
 import fractaldirt from "../assets/guns/ar/holger-556/fractal-dirt.png";
 import magmaheat from "../assets/guns/ar/holger-556/magma-heat.png";
 import paletterosemary from "../assets/guns/ar/holger-556/palette-rosemary.png";
 import topoglitch from "../assets/guns/ar/holger-556/topo-glitch.png";
+// mcw
 import mcw from "../assets/guns/ar/mcw/mcw-call-of-duty-mw3.png";
 import razorfeather from "../assets/guns/ar/mcw/razor-feather.png";
 import skullflagration from "../assets/guns/ar/mcw/skullflagration.png";
 import abyssalfox from "../assets/guns/ar/mcw/abyssal-fox.png";
 import menelaus from "../assets/guns/ar/mcw/menelaus-blue.png";
+// mtz-556
 import mtz556 from "../assets/guns/ar/mtz-556/mtz-556-call-of-duty-mw3.png";
 import orangetones from "../assets/guns/ar/mtz-556/orange-tones.png";
 import irisglitch from "../assets/guns/ar/mtz-556/iris-glitch.png";
 import germfactory from "../assets/guns/ar/mtz-556/germ-factory.png";
 import purple from "../assets/guns/ar/mtz-556/purple.png";
+// mastery
 import gilded from "../assets/mastery/gilded_wm_155.jpg";
 import forged from "../assets/mastery/forged_wm_155.jpg";
 import priceless from "../assets/mastery/priceless_wm_155.jpg";
@@ -47,6 +53,7 @@ export {
   orangetones,
   irisglitch,
   germfactory,
+  //   mastery
   purple,
   gilded,
   forged,
